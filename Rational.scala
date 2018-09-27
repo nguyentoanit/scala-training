150ee9c26928e9322814345b09731bff028930f9
Toan Nguyen <nguyentoandev@gmail.com>
2018-09-27 12:58:36 +0700

    Change method name by mathematical symbol

--- Rational.scala
+++ Rational.scala
@@ -5,7 +5,7 @@ class Rational(n: Int, d: Int) {
     val denom = d / g
     override def toString = numer +"/"+ denom
     def this(n: Int) = this(n, 1) // auxiliary constructor
-    def add(that: Rational): Rational = new Rational(numer * that.denom + that.numer * denom,denom * that.denom)
+    def +(that: Rational): Rational = new Rational(numer * that.denom + that.numer * denom,denom * that.denom)
     def lessThan(that: Rational) = this.numer * that.denom < that.numer * this.denom
     def max(that: Rational) = if (this.lessThan(that)) that else this
     private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
@@ -14,7 +14,7 @@ class Rational(n: Int, d: Int) {
 val rational1 = new Rational(1,2)
 val rational2 = new Rational(3,2)
 val rational3 = new Rational(3)
-val add = rational1 add rational2
+val add = rational1 + rational2
 val lessThan = rational1 lessThan rational2
 val max = rational1 max rational2
 println(add)
