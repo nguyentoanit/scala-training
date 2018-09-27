5e830f53ef719bcccbb92b2f308b15603220b1aa
Toan Nguyen <nguyentoandev@gmail.com>
2018-09-27 11:39:21 +0700

    Auxiliary constructors

--- Rational.scala
+++ Rational.scala
@@ -3,6 +3,7 @@ class Rational(n: Int, d: Int) {
     val numer: Int = n
     val denom: Int = d
     override def toString = numer +"/"+ denom
+    def this(n: Int) = this(n, 1) // auxiliary constructor
     def add(that: Rational): Rational = new Rational(numer * that.denom + that.numer * denom,denom * that.denom)
     def lessThan(that: Rational) = this.numer * that.denom < that.numer * this.denom
     def max(that: Rational) = if (this.lessThan(that)) that else this
@@ -10,9 +11,11 @@ class Rational(n: Int, d: Int) {
 
 val rational1 = new Rational(1,2)
 val rational2 = new Rational(3,2)
+val rational3 = new Rational(3)
 val add = rational1 add rational2
 val lessThan = rational1 lessThan rational2
 val max = rational1 max rational2
 println(add)
 println(lessThan)
 println(max)
+println(rational3)
