8b122f1e31e2155283486fbbf4f175cf14d0cc66
Toan Nguyen <nguyentoandev@gmail.com>
2018-09-27 14:07:45 +0700

    overloaded methods

--- Rational.scala
+++ Rational.scala
@@ -4,10 +4,14 @@ class Rational(n: Int, d: Int) {
     val numer = n / g
     val denom = d / g
     override def toString = numer +"/"+ denom
+
     def this(n: Int) = this(n, 1) // auxiliary constructor
     def +(that: Rational): Rational = new Rational(numer * that.denom + that.numer * denom,denom * that.denom)
+    def - (that: Rational): Rational = new Rational(numer * that.denom - that.numer * denom, denom * that.denom)
+    def - (i: Int): Rational = new Rational(numer - i * denom, denom)
     def lessThan(that: Rational) = this.numer * that.denom < that.numer * this.denom
     def max(that: Rational) = if (this.lessThan(that)) that else this
+
     private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
 }
 
