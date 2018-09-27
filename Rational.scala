c625b03fca697ead5809d25d7000b07570999904
Toan Nguyen <nguyentoandev@gmail.com>
2018-09-27 10:58:07 +0700

    lest than method

--- Rational.scala
+++ Rational.scala
@@ -4,9 +4,12 @@ class Rational(n: Int, d: Int) {
     val denom: Int = d
     override def toString = numer +"/"+ denom
     def add(that: Rational): Rational = new Rational(numer * that.denom + that.numer * denom,denom * that.denom)
+    def lessThan(that: Rational) = this.numer * that.denom < that.numer * this.denom
 }
 
 val rational1 = new Rational(1,2)
 val rational2 = new Rational(3,2)
-val result = rational1 add rational2
-println(result)
+val add = rational1 add rational2
+val lessThan = rational1 lessThan rational2
+println(add)
+println(lessThan)
