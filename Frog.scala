13be2dbd4cfc6a93b4305c4d472096e3f598cbc8
Toan Nguyen <nguyentoandev@gmail.com>
2018-09-28 13:17:27 +0700

    Simple Traits

---
+++ Frog.scala
@@ -0,0 +1,14 @@
+trait Philosophical {
+    def philosophize() {
+        println("I consume memory, therefore I am!")
+    }
+}
+
+class Animal
+trait HasLegs
+class Frog extends Animal with Philosophical with HasLegs {
+    override def toString = "green"
+}
+
+val phrog = new Frog
+phrog.philosophize()
