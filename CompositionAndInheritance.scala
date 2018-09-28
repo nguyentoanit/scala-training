53f0c8226cdcfbedf9c6132a65cda5b989b38c1d
Toan Nguyen <nguyentoandev@gmail.com>
2018-09-28 10:28:13 +0700

    Polymorphism

--- CompositionAndInheritance.scala
+++ CompositionAndInheritance.scala
@@ -10,8 +10,18 @@ class ArrayElement(conts: Array[String]) extends Element {
 
 class LineElement(s: String) extends ArrayElement(Array(s)) {
     override def width = s.length
-    override def height = 1
+    def hidden = 2
 }
 
-val ae = new LineElement("abc")
-println(ae.contents)
+class UniformElement(ch: Char, override val width: Int, override val height: Int) extends Element {
+    private val line = ch.toString * width
+    def contents = Array.fill(height)(line)
+}
+
+val e1 = new ArrayElement(Array("hello", "world"))
+val e2 = new LineElement("hello")
+val e3 = new UniformElement('x', 2, 3)
+
+println(e1.contents)
+println(e2.contents)
+println(e3.contents)
