1e72395804442dba139ca329810a4c42787969e9
Toan Nguyen <nguyentoandev@gmail.com>
2018-10-01 15:11:09 +0700

    matches any number of elements within a sequence

--- SequencePatterns.scala
+++ SequencePatterns.scala
@@ -3,3 +3,9 @@ val expr = List(0, 1, 2);
     case List(0, _, _) => println("Found it")
     case _ => println("Not Found")
 }
+
+// matches any number of elements within a sequence
+expr match {
+    case List(0, _*) => println("Found it")
+    case _ => println("Not Found")
+}
