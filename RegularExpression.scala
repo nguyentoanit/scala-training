b2e1cd2ab2ac7ec2ef67ae8d81752d56b9ac0a4e
Toan Nguyen <nguyentoandev@gmail.com>
2018-10-02 15:11:04 +0700

    Extracting with regular expressions

--- RegularExpression.scala
+++ RegularExpression.scala
@@ -2,3 +2,12 @@ val Decimal = """(-)?(\d+)(\.\d*)?""".r
 val input = "for -1.0 to 99 by 3 2.123123 "
 println(Decimal findAllIn input)
 for (s <- Decimal findAllIn input) println(s)
+
+// Extracting with regular expressions
+val Decimal(sign, integerpart, decimalpart) = "-1.23"
+println(sign)
+println(integerpart)
+println(decimalpart)
+
+for (Decimal(s, i, d) <- Decimal findAllIn input)
+println("sign: "+ s +", integer: "+ i +", decimal: "+ d)
