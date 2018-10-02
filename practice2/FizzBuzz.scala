0c6d7c09b8bba83b95e6e1f2032433625708a434
Toan Nguyen <nguyentoandev@gmail.com>
2018-10-02 14:09:48 +0700

    Use Pattern Matching on Practice 2

--- practice2/FizzBuzz.scala
+++ practice2/FizzBuzz.scala
@@ -1,18 +1,10 @@
-class FizzBuzz {
-    def three = "Fizz"
-    def five = "Buzz"
-    def calculate (number: Int) = {
-        if (number % 15 == 0) {
-            this.three + this.five
-        } else if (number % 5 == 0) {
-            this.five
-        } else if (number % 3 == 0) {
-            this.three
-        } else number
-    }
+def fizzBuzz(number: Int) = (number % 3, number % 5) match {
+    case(0, 0) => "FizzBuzz"
+    case(0, _) => "Fizz"
+    case(_, 0) => "Buzz"
+    case _ => number
 }
 
-val fB = new FizzBuzz
 for(i <- 1 to 30){
-    println(fB.calculate(i))
+    println(fizzBuzz(i))
 }
