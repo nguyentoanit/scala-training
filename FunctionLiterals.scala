603e92521f9fbea6e2af971feca6553f3bb2018e
Toan Nguyen <nguyentoandev@gmail.com>
2018-10-03 11:11:41 +0700

    Placeholder syntax

--- FunctionLiterals.scala
+++ FunctionLiterals.scala
@@ -8,3 +8,8 @@ increase(123123)
 
 val someNumbers = List(-11, -10, -5, 0, 5, 10)
 println(someNumbers.filter((x: Int) => x > 0))
+
+// Placeholder syntax
+println(someNumbers.filter(_ > 0))
+val f = (_: Int) + (_: Int)
+println(f(5, 10))
