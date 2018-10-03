77630a32e19938273ca209f6e41899aec56f735b
Toan Nguyen <nguyentoandev@gmail.com>
2018-10-03 13:42:12 +0700

    Default parameters

--- Closures.scala
+++ Closures.scala
@@ -4,5 +4,15 @@ def sum(numbers: Int*) = {
     for (number <- numbers) sum += number
     sum
 }
-
 println(sum(1,2,3,4))
+
+val arr = Array(1,2,5,6)
+println(sum(arr: _*))
+
+// Default parameter values
+def hello(name: String = "Toan") = {
+    println(s"Hello, ${name}")
+}
+hello()
+hello("")
+hello("ABC")
