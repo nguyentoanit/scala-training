a8131ab691cfeb457ebcfcd05471f9de00108024
Toan Nguyen <nguyentoandev@gmail.com>
2018-10-05 09:44:47 +0700

    List patterns

--- Lists.scala
+++ Lists.scala
@@ -2,3 +2,10 @@ var numbers = (1 :: 2 :: 3 :: 4 :: Nil)
 
 println(numbers.head)
 println(numbers.tail)
+println(numbers.isEmpty)
+
+// List patterns
+var names = "Nguyen" :: "Toan" :: Nil
+var List(lastName, firstName) = names
+println(lastName)
+println(firstName)
