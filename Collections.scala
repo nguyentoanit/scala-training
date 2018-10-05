8e592edbefde02650c3322d6babf8b21a6c762d9
Toan Nguyen <nguyentoandev@gmail.com>
2018-10-05 14:02:18 +0700

    Maps example

--- Collections.scala
+++ Collections.scala
@@ -3,7 +3,9 @@ val fiveToOne = Array(5, "4", 3, true, 1)
 
 // Using maps
 import  scala.collection.mutable.Map
-var nums = Set(1, 2, 3)
-nums += 5
-nums -= 3
-println(nums)
+var information:Map[String, String] = Map()
+information("username") = "username1"
+information("password") = "password1"
+information("email") = "user@example.com"
+information += ("gender" -> "1")
+println(information)
