71cd7cb4edc3dd005306d3da4feb0635eaafdfee
Toan Nguyen <nguyentoandev@gmail.com>
2018-10-05 15:28:46 +0700

    Convert between mutable and immuable

--- Collections.scala
+++ Collections.scala
@@ -9,3 +9,10 @@ information("password") = "password1"
 information("email") = "user@example.com"
 information += ("gender" -> "1")
 println(information)
+
+// // Converting between mutable and immutable sets and maps
+import  scala.collection.mutable.Map
+val muta = scala.collection.mutable.Map("i" -> 1, "ii" -> 2)
+val immu = scala.collection.immutable.Map.empty ++ muta
+println(immu)
+
