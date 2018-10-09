27cba5e5a153aac6a26939bf464d52cc9e237130
Toan Nguyen <nguyentoandev@gmail.com>
2018-10-09 13:46:57 +0700

    Iterator example

---
+++ Iterators.scala
@@ -0,0 +1,4 @@
+// Interators
+println("===========Interators==============")
+val days = Iterator("Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday", "Sunday")
+while(days.hasNext) println (days.next)
