fe2866f4ddeff819576eab8795b07b30ac77853b
Toan Nguyen <nguyentoandev@gmail.com>
2018-10-11 11:15:11 +0700

    nonvariants class

--- TypeParameters.scala
+++ TypeParameters.scala
@@ -18,3 +18,15 @@ object Queue {
     def enqueue(x: T) = new QueueImpl(leading, x :: trailing)
     }
 }
+
+// A nonvariant (rigid) Cell class.
+class Cell[T](init: T) {
+    private[this] var current = init
+    def get = current
+    def set(x: T) { current = x }
+}
+val c1 = new Cell[String]("abc")
+val c2: Cell[String] = c1
+c2.set("1")
+val s: String = c1.get
+println(s)
