dda9b041683c5d4d9800801d877cbeae7901df47
Toan Nguyen <nguyentoandev@gmail.com>
2018-10-11 13:55:45 +0700

    Abstract vals

--- AbstractMembers.scala
+++ AbstractMembers.scala
@@ -10,3 +10,17 @@ class Concrete extends Abstract {
     val initial = "hi"
     var current = initial
 }
+
+// Abstract vals
+abstract class Fruit {
+    val v: String // ‘v’ for value
+    def m: String // ‘m’ for method
+}
+abstract class Apple extends Fruit {
+    val v: String
+    val m: String // OK to override a ‘def’ with a ‘val’
+}
+abstract class BadApple extends Fruit {
+    def v: String // ERROR: cannot override a ‘val’ with a ‘def’
+    def m: String
+}
