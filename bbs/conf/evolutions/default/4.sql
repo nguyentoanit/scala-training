451ddce443c62f80c34138e87db68a6dc3470fc9
Toan Nguyen <nguyentoandev@gmail.com>
2018-10-22 13:00:39 +0700

    Add route for post detail screen and sample data

---
+++ bbs/conf/evolutions/default/4.sql
@@ -0,0 +1,12 @@
+# Example data
+# --- !Ups
+-- Insert rows into table 'post'
+INSERT INTO user
+( -- columns to insert data into
+ email, password, fullname
+)
+VALUES
+    ("user1@gmail.com", "user1@123", "User 1"),
+    ("user2@gmail.com", "user2@123", "User 2")
+# --- !Downs
+DELETE FROM user
