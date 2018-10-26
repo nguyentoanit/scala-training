29970853af65041432381a3f5b5b3e5f975500cb
Toan Nguyen <nguyentoandev@gmail.com>
2018-10-26 14:23:56 +0700

    Add route, model and view for post detail screen

--- bbs/app/controllers/PostsController.scala
+++ bbs/app/controllers/PostsController.scala
@@ -24,4 +24,14 @@ class PostsController @Inject() (cc: ControllerComponents) extends AbstractContr
     val posts = Posts.findAll()
     Ok(views.html.posts(posts))
   }
+
+  // Get Post ID and show detail of Post
+  def detail(id: Long) = Action { implicit request: Request[AnyContent] =>
+    // Find post by ID
+    val post = Post.find(id)
+
+    // Check size of post
+    val template = if (post.size == 1) views.html.post_detail(post) else views.html.code404()
+    Ok(template)
+  }
 }
--- bbs/app/models/Posts.scala
+++ bbs/app/models/Posts.scala
@@ -2,34 +2,31 @@ package models
 
 import scalikejdbc._
 
-case class Posts(id: Long, user_id: Long, title: String, content: String)
+case class Post(id: Long, title: String, content: String, userid: Long, email: String)
+case class User(id: Long, email: String)
 
-object Posts extends SQLSyntaxSupport[Posts] {
+object User extends SQLSyntaxSupport[User]
 
-  // If the table name is same as snake_case'd name of this companion object,
-  // you don't need to specify tableName explicitly.
-  override val tableName = "posts"
-  override val columns = Seq("id", "user_id", "title", "content")
+object Post extends SQLSyntaxSupport[Post] {
 
-  val p = Posts.syntax("p")
+  val (p,u) = (Post.syntax("p"), User.syntax("u"))
 
-  // If you use NamedDB for this entity, override connectionPoolName
-  //override val connectionPoolName = 'anotherdb
+  def apply(p: SyntaxProvider[Post], u:SyntaxProvider[User])(rs: WrappedResultSet): Post = apply(p.resultName, u.resultName)(rs)
+  def apply(p: ResultName[Post], u: ResultName[User])(rs: WrappedResultSet): Post = new Post(
+    id = rs.get(p.id),
+    title = rs.get(p.title),
+    content = rs.get(p.content),
+    userid = rs.get(p.userid),
+    email = rs.get(u.email)
+  )
 
-  def apply(s: SyntaxProvider[Posts])(rs: WrappedResultSet): Posts = apply(s.resultName)(rs)
-  def apply(s: ResultName[Posts])(rs: WrappedResultSet): Posts = new Posts(
-    id = rs.get(s.id),
-    user_id = rs.get(s.user_id),
-    title = rs.get(s.title),
-    content = rs.get(s.content))
-
-  def findAll()(implicit session: DBSession = autoSession): List[Posts] = sql"SELECT ${p.result.*} FROM ${Posts.as(p)}".map(Posts(p)).list.apply()
-
-  def create(p: Posts)(implicit session: DBSession = autoSession) = withSQL {
-    insert.into(Posts).namedValues(
-      column.user_id -> p.user_id,
-      column.title -> p.title,
-      column.content -> p.content
-    )
-  }.update.apply()
+  // Find posts
+  def find(id: Long = 0)(implicit session: DBSession = autoSession): List[Post] = id match {
+    case 0 => withSQL {
+        select.from(Post as p).leftJoin(User as u).on(p.userid, u.id)
+      }.map(Post(p, u)).list.apply()
+    case _ => withSQL {
+        select.from(Post as p).leftJoin(User as u).on(p.userid, u.id).where.eq(p.id, id)
+      }.map(Post(p, u)).list.apply()
+  }
 }
