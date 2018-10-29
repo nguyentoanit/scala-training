70ed991ad602d2d6f6ed5c7809d0b9c4f39ac249
Toan Nguyen <nguyentoandev@gmail.com>
2018-10-29 09:37:19 +0700

    Update test scripts for Posts model

--- bbs/test/models/PostsSpecs.scala
+++ bbs/test/models/PostsSpecs.scala
@@ -5,8 +5,6 @@ import play.api.test.Helpers._
 import org.specs2.mutable._
 import org.specs2.specification.AfterAll
 import scalikejdbc._
-import scalikejdbc.config._
-import scalikejdbc.specs2.AutoRollback
 import traits.DBTestTrait
 import play.api.db.evolutions.Evolutions
 
@@ -22,11 +20,19 @@ object PostsSpecs extends Specification with DBTestTrait with AfterAll {
   }
 
   "Posts Model Test With data" >> {
-    val posts = Posts.findAll()
     "When get all Posts then result type is List[Posts]" >> {
+      val posts = Posts.findAll()
       val result = posts match {
         case _: List[Posts] => true
         case _              => false
+      }
+      result must beTrue
+    }
+    "When get a Post then result type is Option[Posts]" >> {
+      val post = Posts.find(1)
+      val result = post match {
+        case _: Option[Posts] => true
+        case _                => false
       }
       result must beTrue
     }
@@ -42,6 +48,17 @@ object PostsSpecs extends Specification with DBTestTrait with AfterAll {
       val result = posts match {
         case _: List[Posts] => true
         case _              => false
+      }
+      result must beTrue
+    }
+    val post = Posts.find(1)
+    "When get a Post then Records's amount == 0" >> {
+      post.size == 0
+    }
+    "When get a Post then result is None" >> {
+      val result = post match {
+        case Some(_) => false
+        case None    => true
       }
       result must beTrue
     }
