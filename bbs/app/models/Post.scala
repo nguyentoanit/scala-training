cda5e2c1b7a28ace859ba0da3ed756c368a995b4
Toan Nguyen <nguyentoandev@gmail.com>
2018-10-31 11:29:55 +0700

    Update apply function of Model

--- bbs/app/models/Post.scala
+++ bbs/app/models/Post.scala
@@ -10,12 +10,11 @@ object Post extends SQLSyntaxSupport[Post] {
   override val tableName = "posts"
   override val columns = Seq("id", "email", "title", "content")
 
-  def apply(p: SyntaxProvider[Post])(rs: WrappedResultSet): Post = apply(p.resultName)(rs)
-  def apply(p: ResultName[Post])(rs: WrappedResultSet): Post = new Post(
-    id = rs.get(p.id),
-    email = rs.get(p.email),
-    title = rs.get(p.title),
-    content = rs.get(p.content)
+  def apply(p: SyntaxProvider[Post])(rs: WrappedResultSet): Post = new Post(
+    id = rs.get(p.resultName.id),
+    email = rs.get(p.resultName.email),
+    title = rs.get(p.resultName.title),
+    content = rs.get(p.resultName.content)
   )
 
   //Get all posts
@@ -24,8 +23,12 @@ object Post extends SQLSyntaxSupport[Post] {
   }.map(Post(p)).list.apply()
 
   // Get post by post id
-  def getPostByID(id: Long)(implicit session: DBSession = autoSession): Option[Post] = withSQL {
+  def getByID(id: Long)(implicit session: DBSession = autoSession): Option[Post] = withSQL {
     select.from(Post as p).where.eq(p.id, id)
   }.map(Post(p)).single.apply()
 
+  def create(p: Post)(implicit session: DBSession = autoSession) = withSQL {
+    val c = Post.column
+    insert.into(Post).columns(c.email, c.title, c.content).values(p.email, p.title, p.content)
+  }.update.apply()
 }
